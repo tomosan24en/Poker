7481b9acb6d251be0dca6fe8fd3af960de6dd36e
tomosan24en <tomo.kyoto804.en@gmail.com>
2025-04-30 18:19:50 +0900

    テンプレ

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 import pyxel
-from .card import *
+from card import *
+
+
+class App:
+    def __init__(self) -> None:
+        pyxel.init(200, 200, title="Poker")
+        pyxel.load("./resource.pyxres")
+        self.initialize()
+        pyxel.run(self.update, self.draw)
+
+    def initialize(self) -> None:
+        pass
+
+    def update(self) -> None:
+        pass
+
+    def draw(self) -> None:
+        pass
+
+
+App()
