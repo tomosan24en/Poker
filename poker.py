68103ccda277f62b602c8c3ac57c2afeddbdf27f
tomosan24en <tomo.kyoto804.en@gmail.com>
2025-04-30 23:14:31 +0900

    DealtCardsクラスを作成

--- poker.py
+++ poker.py
@@ -3,29 +3,38 @@ from abc import ABCMeta, abstractmethod
 
 from card import Card
 
-def _contain_joker(cards: list[Card]) -> int:
-    return any(1 if card.is_joker() else 0 for card in cards)
 
-def _count_jokers(cards: list[Card]) -> int:
-    return sum(1 if card.is_joker() else 0 for card in cards)
+class DealtCards:
+    def __init__(self, cards: list[Card]) -> None:
+        self.cards = cards
+        self._jokers = sum(1 if card.is_joker() else 0 for card in self.cards)
 
-def _group_by_number(cards: list[Card]) -> dict[Number, list[Card]]:
-    cards_except_joker = [card for card in cards if not card.is_joker()]
-    if not cards_except_joker:
-        return {}
-    res: dict[Number, list[Card]] = {}
-    for i, card in enumerate(cards_except_joker):
-        if card.get_number() in res.keys():
-            res[card.get_number()].append(card)
-        else:
-            res[card.get_number()] = [card]
-    return res
+        grouping: dict[Number, list[Card]] = {}
+        cards_except_joker = [card for card in self.cards if not card.is_joker()]
+        for i, card in enumerate(cards_except_joker):
+            if card.get_number() in grouping.keys():
+                grouping[card.get_number()].append(card)
+            else:
+                grouping[card.get_number()] = [card]
+        self._grouping = grouping
 
-def _count_groups(gruoping: dict[Number, list[Card]], above: int) -> int:
-    return sum(
-        1 if len(number_card_list) >= above else 0
-        for number_card_list in gruoping.values()
-    )
+    def contains_joker(self) -> int:
+        return self._jokers >= 1
+
+    def count_joker(self) -> int:
+        return self._jokers
+
+    def group_by_number(self) -> dict[Number, list[Card]]:
+        return self._grouping
+    
+    def count_distinct_numbers(self) -> int:
+        return len(self._grouping.keys())
+
+    def count_groups(self, above: int) -> int:
+        return sum(
+            1 if len(number_card_list) >= above else 0
+            for number_card_list in self._grouping.values()
+        )
 
 
 class Hand(metaclass=ABCMeta):
@@ -34,7 +43,7 @@ class Hand(metaclass=ABCMeta):
         self.description = description
 
     @abstractmethod
-    def check(self, cards: list[Card]) -> bool:
+    def check(self, cards: DealtCards) -> bool:
         pass
 
 
@@ -42,54 +51,50 @@ class _OnePair(Hand):
     def __init__(self) -> None:
         super().__init__("ワンペア", "同じ数字のカードの2枚の組が1組")
 
-    def check(self, cards: list[Card]) -> bool:
-        if _contain_joker(cards):
+    def check(self, cards: DealtCards) -> bool:
+        if cards.contains_joker():
             return True
-        
-        grouping = _group_by_number(cards)
-        return _count_groups(grouping, above=2) >= 1
+        return cards.count_groups(2) >= 1
         
 
 class _TwoPairs(Hand):
     def __init__(self) -> None:
         super().__init__("ツーペア", "同じ数字のカードの2枚の組が2組")
 
-    def check(self, cards: list[Card]) -> bool:
-        jokers = _count_jokers(cards)
+    def check(self, cards: DealtCards) -> bool:
+        jokers = cards.count_joker()
         if jokers >= 2:
             return True
-        groupings = _group_by_number(cards)
         if jokers == 1:
-            return len(groupings.keys()) >= 2 and _count_groups(groupings, 2) >= 1
+            return cards.count_distinct_numbers() >= 2 and cards.count_groups(2) >= 1
         else:
-            return _count_groups(groupings, 2) >= 2
+            return cards.count_groups(2) >= 2
         
 
 class _ThreeCards(Hand):
     def __init__(self) -> None:
         super().__init__("スリーカード", "同じ数字のカードが3枚")
 
-    def check(self, cards: list[Card]) -> bool:
-        jokers = _count_jokers(cards)
+    def check(self, cards: DealtCards) -> bool:
+        jokers = cards.count_joker()
         if jokers >= 2:
             return True
-        groupings = _group_by_number(cards)
-        return _count_groups(groupings, 3 - jokers) >= 1
+        return cards.count_groups(3 - jokers) >= 1
     
 
 ONE_PAIR = _OnePair()
 TWO_PAIRS = _TwoPairs()
 THREE_CARDS = _ThreeCards()
 
-HANDS = [
+HANDS: list[Hand] = [
     ONE_PAIR,
     TWO_PAIRS,
     THREE_CARDS,
 ]
 
 for _ in range(10):
-    d = Deck(jokers=5).random_pick(5)
-    print_card_list(d)
+    d = DealtCards(Deck(jokers=5).random_pick(5))
+    print_card_list(d.cards)
     for hand in HANDS:
         print(hand.name, hand.check(d))
     print("")
